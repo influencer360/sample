3cf646ef93059ed64bdf09f619628682c524d6bc
Kunal Sekhri <kunalsekhri123@gmail.com>
2024-02-14 07:15:47 -0800

    Base sample

---
+++ my-app/src/app/streams/page.tsx
@@ -0,0 +1,13 @@
+
+
+'use server';
+
+
+export default async function Page() {
+
+  return (
+    <div className="flex min-h-screen flex-col">
+      Welcome to Streamz page.
+    </div>
+  )
+}
---
+++ my-app/src/components/Button/SampleButton.tsx
@@ -0,0 +1,12 @@
+
+
+'use client';
+
+import Button from '@mui/material/Button';
+
+export default function SampleButton() {
+
+  return (
+    <Button>Sample Button</Button>
+  )
+}
